b634140ab99cbff6c68efc5ef08bcbf1380e442e
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-07-19 21:00:05 -0500

    - Allow users to add multiple recipients while sending email. - Add CC and BCC options. - Check if attachment file is available before trying to attach. - Wrap recipient, cc and bcc items in a single list before email kick off. - Remove sender arg and default to the user login email address. - Fix version number format.

--- gmailconnector/send_email.py
+++ gmailconnector/send_email.py
@@ -1,6 +1,7 @@
 from email.mime import multipart, text
 from email.mime.application import MIMEApplication
 from logging import INFO, basicConfig, getLogger
+from os.path import isfile, realpath
 from pathlib import PurePath
 from smtplib import SMTP, SMTPAuthenticationError, SMTPConnectError
 
@@ -18,21 +19,25 @@ class SendEmail:
         - gmail_pass: Password to authenticate TLS session.
         - recipient: Email address of the recipient to whom the email has to be sent.
         - subject: Subject line of the email.
-        - attachment [Optional] : Filename that has to be attached.
         - body [Optional] : Body of the email. Defaults to no body.
-        - sender [Optional] : Email address of the sender. Defaults to gmail_user.
+        - attachment [Optional] : Filename that has to be attached.
+        - cc [Optional]: Email address of the recipient to whom the email has to be CC'd.
+        - bcc [Optional]: Email address of the recipient to whom the email has to be BCC'd.
 
     """
 
-    def __init__(self, gmail_user: str, gmail_pass: str, recipient: str, subject: str,
-                 attachment: str = None, body: str = None, sender: str = None):
+    def __init__(self, gmail_user: str, gmail_pass: str, recipient: str or list,
+                 subject: str, body: str = None, attachment: str = None,
+                 cc: str or list = None, bcc: str or list = None):
         """Gathers all the necessary parameters to send an email."""
         self.gmail_user = gmail_user
         self.gmail_pass = gmail_pass
         self.recipient = recipient
         self.subject = subject
+        self.cc = cc
+        self.bcc = bcc
         self.body = body
-        self.sender = f"GmailConnector <{sender}>" if sender else f"GmailConnector <{gmail_user}>"
+        self.sender = f"GmailConnector <{gmail_user}>"
         self.attachment = attachment
         self.server = SMTP('smtp.gmail.com')
 
@@ -43,7 +48,9 @@ class SendEmail:
             self.server.quit()
 
     def multipart_message(self) -> multipart.MIMEMultipart:
-        """Creates a multipart message with the subject, body, from and to address, and attachment if passed.
+        """Creates a multipart message with subject, body, from and to address, and attachment if filename is passed.
+
+        Logs a message if a filename is given for attachment but not available at the right path.
 
         Returns:
             `multipart.MIMEMultipart`:
@@ -53,16 +60,20 @@ class SendEmail:
         msg = multipart.MIMEMultipart()
         msg['Subject'] = self.subject
         msg['From'] = self.sender
-        msg['To'] = self.recipient
+        msg['To'] = ','.join(self.recipient) if isinstance(self.recipient, list) else self.recipient
+        if cc := self.cc:
+            msg['Cc'] = cc
 
         if body := self.body:
             msg.attach(payload=text.MIMEText(body))
 
-        if filename := self.attachment:
+        if (filename := self.attachment) and (isfile(filename)):
             with open(filename, 'rb') as attachment:
                 attribute = MIMEApplication(attachment.read(), _subtype=filename.split('.')[-1])
             attribute.add_header('Content-Disposition', 'attachment', filename=filename)
             msg.attach(payload=attribute)
+        elif filename:
+            logger.warning(f"{filename} is unavailable at {realpath(filename='')}\nProceeding without the attachment.")
 
         return msg
 
@@ -84,9 +95,18 @@ class SendEmail:
             logger.error("Error during connection establishment with GMAIL server.")
             return
 
+        to = self.recipient
+        recipients = [to] if isinstance(to, str) else to
+
+        if cc := self.cc:
+            recipients.append(cc) if isinstance(cc, str) else recipients.extend(cc)
+
+        if bcc := self.bcc:
+            recipients.append(bcc) if isinstance(bcc, str) else recipients.extend(bcc)
+
         self.server.sendmail(
             from_addr=self.sender,
-            to_addrs=[self.recipient],
+            to_addrs=recipients,
             msg=self.multipart_message().as_string()
         )
 
@@ -99,6 +119,6 @@ if __name__ == '__main__':
 
     email_obj = SendEmail(
         gmail_user=environ.get('gmail_user'), gmail_pass=environ.get('gmail_pass'), recipient=environ.get('recipient'),
-        subject=datetime.now().strftime("%B %d, %Y %I:%M %p"), attachment=None, body=None, sender=None
+        subject=datetime.now().strftime("%B %d, %Y %I:%M %p")
     )
     print(email_obj.send_email())
--- version.py
+++ version.py
@@ -1,1 +1,1 @@
-version_info = (0, 0, 18)
+version_info = (0, 1, 9)
